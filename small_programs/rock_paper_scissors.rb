68af144659953ac309de3f11cede50de488d640e
Jason Slusarchuk <jason.slusarchuk@gmail.com>
2017-08-18 10:36:25 -0700

    added support for lizard and spock options; added best of 5 rounds to win

--- small_programs/rock_paper_scissors.rb
+++ small_programs/rock_paper_scissors.rb
@@ -1,10 +1,16 @@
-# A simple game of rock-paper-scissors.
+# A simple game of rock-paper-scissors-lizard-spock.
 
-CHOICES = %w(rock paper scissors)
+CHOICES = %w(rock paper scissors lizard spock)
+LOSING_CONDITIONS = { rock: %w(lizard scissors),
+                      paper: %w(rock spock),
+                      scissors: %w(paper lizard),
+                      lizard: %(spock paper),
+                      spock: %(scissors rock) }
+player_score = 0
+computer_score = 0
 
-# main program loop start
+# main game loop start
 loop do
-
   # player selects an option
   player_choice = ""
   loop do
@@ -20,22 +26,38 @@ loop do
   puts "The computer chose #{computer_choice}..."
 
   # determine winner
-  case
-  when player_choice == computer_choice
+  if player_choice == computer_choice
     puts "It's a tie!"
-  when (player_choice == "rock" && computer_choice == "scissors") ||
-       (player_choice == "paper" && computer_choice == "rock") ||
-       (player_choice == "scissors" && computer_choice == "paper")
-    puts "Player wins!"
+  elsif (player_choice == "rock" &&
+          LOSING_CONDITIONS[:rock].include?(computer_choice)) ||
+        (player_choice == "paper" &&
+          LOSING_CONDITIONS[:paper].include?(computer_choice)) ||
+        (player_choice == "scissors" &&
+          LOSING_CONDITIONS[:scissors].include?(computer_choice)) ||
+        (player_choice == "lizard" &&
+          LOSING_CONDITIONS[:lizard].include?(computer_choice)) ||
+        (player_choice == "spock" &&
+          LOSING_CONDITIONS[:spock].include?(computer_choice))
+    player_score += 1
+    puts "Player wins the round!"
   else
-    puts "Computer wins!"
+    computer_score += 1
+    puts "Computer wins the round!"
   end
 
-  # ask if user would like to play agian
-  puts "Would you like to play again? (y/n)"
-  player_input = gets.chomp
-  break if player_input.downcase[0] != "y"
-
+  # test for winner, play again?
+  if player_score == 5 || computer_score == 5
+    if player_score == 5
+      puts "You win the game!!!"
+    else
+      puts "The computer wins!"
+    end
+    puts "Would you like to play again? (y/n)"
+    player_input = gets.chomp
+    break if player_input.downcase[0] != "y"
+    player_score = 0
+    computer_score = 0
+  end
 end
 
 # game ends
