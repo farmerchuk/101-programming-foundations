863f2104eeeab6d36ac3d0993145a501b44e4086
Jason Slusarchuk <jason.slusarchuk@gmail.com>
2017-08-24 14:56:39 -0700

    added better omputer ai

--- slightly_larger_programs/tic_tac_toe.rb
+++ slightly_larger_programs/tic_tac_toe.rb
@@ -1,5 +1,4 @@
 # a simple game of tic-tac-toe
-require 'pry'
 
 TOKENS = ['X', 'O', '@', '#']
 WINNING_CONDITIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
@@ -35,7 +34,7 @@ def display_score(player_list)
     score = player_list[n + 1][:score]
     puts "#{name} has #{score} points!"
   end
-  puts "(First to 3 wins)"
+  puts '(First to 3 wins)'
   puts
 end
 
@@ -53,12 +52,12 @@ def validate_choice(board, square)
   board[square] == square
 end
 
-def how_many_players
+def how_many_humans
   players = 0
   loop do
     print 'How many players? (up to 4): '
     players = gets.chomp.to_i
-    break if (1..4).include?(players)
+    break if (1..4).cover?(players)
   end
   players
 end
@@ -68,14 +67,14 @@ def get_player_name(count)
   gets.chomp
 end
 
-def create_players(num_of_players)
+def create_players(num_of_humans)
   player_list = {}
-  if num_of_players > 1
-    num_of_players.times do |n|
+  if num_of_humans > 1
+    num_of_humans.times do |n|
       player_list[n + 1] = { name: get_player_name(n + 1), token: TOKENS[n],
-                         ai: false, score: 0 }
+                             ai: false, score: 0 }
     end
-  elsif num_of_players <= 1
+  elsif num_of_humans <= 1
     player_list[1] = { name: get_player_name(1), token: TOKENS[0],
                        ai: false, score: 0 }
     player_list[2] = { name: 'Computer', token: TOKENS[1],
@@ -85,19 +84,48 @@ def create_players(num_of_players)
 end
 
 def player_selects_square(board, player, player_list)
-  input = ''
+  square = ''
   if player[:ai]
     sleep 1
-    input = empty_squares(board, player_list).values.sample
+    square = computer_selects_square(board, player_list)
   else
     loop do
       print "#{player[:name]}, please choose a square: "
-      input = gets.chomp.to_i
-      break if validate_choice(board, input)
+      square = gets.chomp.to_i
+      break if validate_choice(board, square)
       puts 'Sorry, that is not a valid option!'
     end
   end
-  mark_square(board, input, player[:token])
+  mark_square(board, square, player[:token])
+end
+
+def computer_selects_square(board, player_list)
+  square = computer_selects_aggressive(board, player_list)
+  square = computer_selects_defensive(board, player_list) unless square
+  square = empty_squares(board, player_list).values.sample unless square
+  square
+end
+
+def computer_selects_aggressive(board, player_list)
+  WINNING_CONDITIONS.each do |line|
+    matches_comp = line.count { |sqr| board[sqr] == player_list[2][:token] }
+    open_sqrs = line.select { |sqr| board[sqr].class == Integer }.size
+    if matches_comp == 2 && open_sqrs == 1
+      return line.select { |sqr| board[sqr].class == Integer }[0]
+    end
+  end
+  nil
+end
+
+def computer_selects_defensive(board, player_list)
+  WINNING_CONDITIONS.each do |line|
+    matches_human = line.count { |sqr| board[sqr] == player_list[1][:token] }
+    open_sqrs = line.select { |sqr| board[sqr].class == Integer }.size
+    if matches_human == 2 && open_sqrs == 1
+      return line.select { |sqr| board[sqr].class == Integer }[0]
+    end
+  end
+  nil
 end
 
 def winning_line?(board, line, player)
@@ -125,7 +153,7 @@ def empty_squares(board, player_list)
   end
 end
 
-def player_turn(board, player_list)
+def player_turns(board, player_list)
   player_list.count.times do |n|
     display_board(board, player_list)
     player_selects_square(board, player_list[n + 1], player_list)
@@ -136,36 +164,37 @@ def player_turn(board, player_list)
   nil
 end
 
+def prepare_next_round
+  print 'Preparing the next round '
+  4.times do
+    sleep 1
+    print '. '
+  end
+end
 
-loop do  # new game loop
+loop do # new game loop
   system 'clear'
   puts 'Welcome to Tic-Tac-Toe!'
 
-  num_of_players = how_many_players
-  player_list = create_players(num_of_players)
+  num_of_humans = how_many_humans
+  player_list = create_players(num_of_humans)
   round_winner = nil
   board = initialize_board
 
-  loop do  # new round loop
-    loop do
-      break if player_turn(board, player_list)
+  loop do # new round loop
+    loop do # take turns until someone wins
+      break if player_turns(board, player_list)
     end
-
     display_board(board, player_list)
     round_winner = winning_player(board, player_list)
-
     if round_winner
       round_winner[:score] += 1
       break if round_winner[:score] >= 3
       puts "#{round_winner[:name]} wins the round!"
-      print 'Preparing the next round '
-      4.times do
-        sleep 1
-        print '. '
-      end
     else
       puts "It's a tie!"
     end
+    prepare_next_round
     round_winner = nil
     board = initialize_board
   end
